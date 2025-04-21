bae67d7bf77551597b9e2b1b41cdd99c82c0857f
Luis J. Rodriguez O. <ljavierrodriguez@gmail.com>
2025-04-21 11:08:52 -0400

    Update Home.jsx

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,17 @@
-import React from "react";
+import Encabezado from "./Encabezado"
+import Input from "./Input"
+import Lista from "./Lista"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<>
+			<h1>Hola desde React</h1>
+			<Lista />
+			<Input type="password" name="password" placeholder="Password" />
+			<Input type="email" name="Email" placeholder="Email" />
+			<Encabezado texto="Hola Mundo desde Props" />
+		</>
+	)
+}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+export default Home
