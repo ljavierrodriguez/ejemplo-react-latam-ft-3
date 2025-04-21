d691a8c637bd4fea6fa767386c832407a60b188b
Luis J. Rodriguez O. <ljavierrodriguez@gmail.com>
2025-04-21 11:07:41 -0400

    Create Lista.jsx

---
+++ src/js/components/Lista.jsx
@@ -0,0 +1,14 @@
+import ElementoLista from "./ElementoLista"
+
+const Lista = () => {
+    return (
+        <ul>
+            <ElementoLista />
+            <ElementoLista />
+            <ElementoLista />
+            <ElementoLista />
+        </ul>
+    )
+}
+
+export default Lista
